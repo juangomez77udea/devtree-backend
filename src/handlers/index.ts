a2e0d692d690451e1f9e5a728e38a5a609e1708b
Juan Carlos Gomez Jaramillo <juangomez88@gmail.com>
2026-02-21 13:03:44 -0500

    INstalacion de cloudinary y formidable para manejo de imagenes

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,13 @@
 import type { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import slug from 'slug';
+import formidable from 'formidable';
+import { v4 as uuid } from 'uuid'
 import User from '../models/User';
 import { checkPassword, hashPassword } from '../utils/auth';
 import { generateJWT } from '../utils/jwt';
+import cloudinary from '../config/cloudinary';
+import { Error } from 'mongoose';
 
 export const createAccount = async (req: Request, res: Response) => {
 
@@ -66,14 +70,14 @@ export const getUser = async (req: Request, res: Response) => {
 export const updateProfile = async (req: Request, res: Response) => {
       try {
             const { description } = req.body;
-            
+
             const handle = slug(req.body.handle, '')
-            const handleExists = await User.findOne({handle})
-            if(handleExists && handleExists.email !== req.user.email) {
+            const handleExists = await User.findOne({ handle })
+            if (handleExists && handleExists.email !== req.user.email) {
                   const error = new Error('Nombre de usuario no disponible');
-                  return res.status(409).json({error: error.message })
+                  return res.status(409).json({ error: error.message })
             }
-      
+
             //Actualizar el usuario
             req.user.description = description
             req.user.handle = handle
@@ -82,5 +86,28 @@ export const updateProfile = async (req: Request, res: Response) => {
 
       } catch (e) {
             return res.status(500).json({ error: e.message })
+      }
+}
+
+export const uploadImage = async (req: Request, res: Response) => {
+      const form = formidable({ multiples: false })
+
+      try {
+            form.parse(req, (error, fields, files) => {
+                  cloudinary.uploader.upload(files.file[0].filepath, {public_id: uuid()}, async function (error, result) {
+                        if (error) {
+                              const error = new Error('Hubo un error al subir la imagen')
+                              return res.status(500).json({ error: error.message })
+                        }
+                        if(result) {
+                              req.user.image = result.secure_url
+                              await req.user.save()
+                              res.json({ image: result.secure_url })
+                        }
+                  })
+            })
+      } catch (e) {
+            const error = new Error('Hubo un error')
+            return res.status(500).json({ error: error.message })
       }
 }
